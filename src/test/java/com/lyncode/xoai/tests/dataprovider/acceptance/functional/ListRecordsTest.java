243e698475573cf2f537927dd2debb0d83e8509b
João Melo <jmelo@lyncode.com>
2013-10-29 21:46:34 +0000

    Some syntactic changes (acceptance tests)

--- src/test/java/com/lyncode/xoai/tests/dataprovider/acceptance/functional/ListRecordsTest.java
+++ src/test/java/com/lyncode/xoai/tests/dataprovider/acceptance/functional/ListRecordsTest.java
@@ -11,18 +11,21 @@ import org.junit.Test;
 import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 
+import static com.lyncode.xoai.tests.SyntacticSugar.and;
 import static com.lyncode.xoai.tests.SyntacticSugar.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ListRecordsTest extends AbstractDataProviderTest {
     @Test
     public void shouldReturnGivenItems () throws WritingXmlException, OAIException, InvalidContextException, IOException, XMLStreamException, ConfigurationException {
-        given(theItemRepository()
-                .withRandomItems(20));
+        given(theConfiguration().withMaxListRecords(10));
+        and(given(theItemRepository()
+                .withRandomItems(11)));
 
         afterHandling(aRequest().withVerb("ListRecords").withMetadataPrefix(theFormatPrefix()));
 
         assertThat(theResult(), hasXPath("//o:record/o:header/o:identifier"));
+        assertThat(theResult(), hasXPath("//o:resumptionToken"));
     }
 
     private StubbedItem anItem() {
