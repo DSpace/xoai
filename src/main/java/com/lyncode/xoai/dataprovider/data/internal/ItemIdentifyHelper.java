b444f8aa765acf3787454c7483d09d24125c8491
Christian Scheible <christian.scheible@uni-konstanz.de>
2015-09-08 08:29:06 +0200

    Fixes #45 Sets are added to an item idependent from the filter

--- src/main/java/com/lyncode/xoai/dataprovider/data/internal/ItemIdentifyHelper.java
+++ src/main/java/com/lyncode/xoai/dataprovider/data/internal/ItemIdentifyHelper.java
@@ -17,10 +17,15 @@ public class ItemIdentifyHelper {
     public List<ReferenceSet> getSets(XOAIContext context) {
         List<ReferenceSet> list = this.item.getSets();
         for (StaticSet staticSet : context.getStaticSets()) {
-            if (staticSet.hasCondition() && staticSet.getCondition().getFilter().isItemShown(item))
+            if (staticSet.hasCondition() && staticSet.getCondition().getFilter().isItemShown(item))            {
+                // this set has a condition and the current item fulfills it so add the set to the item
                 list.add(staticSet);
-            else
+            }
+            else if(!staticSet.hasCondition())            {
+                // this set has no condition so always add the set to the item
                 list.add(staticSet);
+            }
+            // otherwise: the set has a condition which is not fulfilled by the current item so don't add.
         }
         return list;
     }
