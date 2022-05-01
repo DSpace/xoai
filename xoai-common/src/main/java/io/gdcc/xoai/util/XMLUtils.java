5f749a150086a0872a42c097ccd63a3c672473a2
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-05-01 19:08:15 +0200

    refactor(common): make XMLUtils use try-with-resources for formatting

--- xoai-common/src/main/java/io/gdcc/xoai/util/XMLUtils.java
+++ xoai-common/src/main/java/io/gdcc/xoai/util/XMLUtils.java
@@ -15,25 +15,30 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class XMLUtils {
     static final TransformerFactory factory = TransformerFactory.newInstance();
-
+    
     public static String format(String unformattedXml) {
-        try {
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            Transformer transformer;
-            transformer = factory.newTransformer();
+        try (
+            OutputStream out = new ByteArrayOutputStream();
+            InputStream in = new ByteArrayInputStream(unformattedXml.getBytes())
+        ) {
+            Transformer transformer = factory.newTransformer();
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            
             //initialize StreamResult with File object to save to file
             StreamResult result = new StreamResult(out);
-            StreamSource source = new StreamSource(new ByteArrayInputStream(unformattedXml.getBytes()));
+            StreamSource source = new StreamSource(in);
+            
             transformer.transform(source, result);
             return out.toString();
         } catch (Exception e) {
             return "";
         }
     }
-
+    
 }
