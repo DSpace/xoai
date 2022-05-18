60d21fe1750d7a954a26ec7864e6807aa9ffb083
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-05-18 18:43:51 +0200

    refactor(common): change Metadata to use EchoElement, CopyElement or XOAIMetadata
    All of these share the common interface XmlWritable, so we can store the element to write out
    as metadata as this type (no switch or if necessary).
    
    This also moves the data handling to using classes - they create the data and the XML writables,
    this class is just used for the modeling of a fluent API.
    
    It is linked to the creation of items, which are generated by the application using this library
    via the repository interfaces. The application may decide how to fill in metadata when creating items.

--- xoai-common/src/main/java/io/gdcc/xoai/model/oaipmh/Metadata.java
+++ xoai-common/src/main/java/io/gdcc/xoai/model/oaipmh/Metadata.java
@@ -8,50 +8,69 @@
 
 package io.gdcc.xoai.model.oaipmh;
 
+import io.gdcc.xoai.xml.CopyElement;
 import io.gdcc.xoai.xmlio.exceptions.XmlWriteException;
 import io.gdcc.xoai.model.xoai.XOAIMetadata;
 import io.gdcc.xoai.xml.EchoElement;
 import io.gdcc.xoai.xml.XmlWritable;
 import io.gdcc.xoai.xml.XmlWriter;
 
-import javax.xml.bind.annotation.XmlValue;
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
+/**
+ * Model class representing <OAI-PMH><ListRecords|GetRecord><record><metadata> elements.
+ * The element holds the actual metadata as XML structure serialization, e.g. using OAI Dublin Core
+ */
 public class Metadata implements XmlWritable {
-    private final String string;
-
-    @XmlValue
-    protected final XOAIMetadata value;
-
-    public Metadata(XOAIMetadata value) {
-        this.string = "";
-        this.value = value;
+    
+    private final XmlWritable element;
+    
+    public Metadata(final XOAIMetadata value) {
+        this.element = value;
     }
 
-    public Metadata(String value) {
-        this.string = value;
-        this.value = null;
+    public Metadata(final String value) {
+        this.element = new EchoElement(value);
     }
 
-    public Metadata(InputStream value) throws IOException {
-        this.string = new String(value.readAllBytes(), StandardCharsets.UTF_8);
-        this.value = null;
+    public Metadata(final EchoElement value) {
+        this.element = value;
     }
-
+    
+    public Metadata(final CopyElement value) {
+        this.element = value;
+    }
+    
+    /**
+     * If this metadata element needs to be passed through an {@link io.gdcc.xoai.xml.XSLPipeline},
+     * this is indicated by "true". When this metadata element consists of pregenerated data, which
+     * shall not be processed again, this is indicated by "false".
+     *
+     * @return true - needs a processing pipeline, false - use as is
+     */
+    public boolean needsProcessing() {
+        return !(this.element instanceof CopyElement);
+    }
+    
+    /**
+     * Create a metadata element from an associated {@link InputStream} which will
+     * copy the streams content to the OAI-PMH response when the metadata gets written.
+     * @param stream The input stream containing the metadata
+     * @return A Metadata item to store away, e. g. in an {@link io.gdcc.xoai.dataprovider.model.Item}
+     */
+    public static Metadata copyFromStream(InputStream stream) {
+        return new Metadata(new CopyElement(stream));
+    }
+    
     @Override
-    public void write(XmlWriter writer) throws XmlWriteException {
-        if (this.value != null)
-            this.value.write(writer);
-        else {
-            EchoElement elem = new EchoElement(string);
-            elem.write(writer);
-        }
+    public void write(final XmlWriter writer) throws XmlWriteException {
+        writer.write(element);
     }
 
-    public XOAIMetadata getValue () {
-        return value;
+    public XOAIMetadata getXoaiMetadata() {
+        if (element instanceof XOAIMetadata)
+            return (XOAIMetadata) element;
+        else
+            return null;
     }
-
 }
